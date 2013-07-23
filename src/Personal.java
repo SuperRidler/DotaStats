e170d6fbff3e2d738bb3629a115fd3f1422f16a5
SuperRidler <jamieridler@gmail.com>
2013-07-23 16:39:40 +0100

    Added SQL inserts for database.

--- src/Personal.java
+++ src/Personal.java
@@ -7,6 +7,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -15,6 +17,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 
 public class Personal {
 
@@ -40,23 +44,26 @@ public class Personal {
 		try {
 			Class.forName(driver);
 			con = (Connection) DriverManager.getConnection(url, "root", "");
+			con.setDumpQueriesOnException(true);
+			System.out.println("Database connection setup.");
 		} catch (Exception e) {
 			System.out.println("Failed to connect to the database.");
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	
+
 	public static void closeDB() {
 		try {
 			con.close();
+			System.out.println("Connection to database closed.");
 		} catch (Exception e) {
 			System.out.println("Failed to close the database connection.");
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	
+
 	/* Attach the API key to the URLs for use. */
 	public static void loadKey() {
 		try {
@@ -94,30 +101,37 @@ public class Personal {
 				JsonObject iobj = it.next().getAsJsonObject();
 				matchIds.add(iobj.get("match_id").getAsInt());
 			}
-			matchId = String.valueOf(matchIds.get(matchIds.size()-1) - 1);
+			matchId = String.valueOf(matchIds.get(matchIds.size() - 1) - 1);
 		}
 		System.out.println(matchIds);
-		System.out.println(matchIds.size());	}
+		System.out.println(matchIds.size());
+	}
 
 	/* Get the match details for a match id int. */
 	public static JsonObject getMatchDetails(int matchId) {
 		return getMatchDetails(String.valueOf(matchId));
 	}
-	
+
 	/* Get the match details for a match id string. */
 	public static JsonObject getMatchDetails(String matchId) {
 		return getJsonRequest(detailsAddress + "match_id=" + matchId + "&");
 	}
-	
+
 	/* Use the match ids to load the match data. */
 	public static void loadMatches() {
 		for (Integer i : matchIds) {
 			JsonObject matchInfo = getMatchDetails(i);
 			JsonObject resultObj = matchInfo.getAsJsonObject("result");
 			JsonArray players = resultObj.getAsJsonArray("players");
+			int[] playerKeys = new int[10];
+			int keyPos = 0;
 			for (JsonElement player : players) {
 				JsonObject playerObj = player.getAsJsonObject();
-				int account_id = playerObj.get("account_id").getAsInt();
+				int account_id = 0;
+				/* Game may contain bots. */
+				if (playerObj.has("account_id")) {
+					account_id = playerObj.get("account_id").getAsInt();
+				}
 				int player_slot = playerObj.get("player_slot").getAsInt();
 				int hero_id = playerObj.get("hero_id").getAsInt();
 				int item_0 = playerObj.get("item_0").getAsInt();
@@ -129,7 +143,10 @@ public class Personal {
 				int kills = playerObj.get("kills").getAsInt();
 				int deaths = playerObj.get("deaths").getAsInt();
 				int assists = playerObj.get("assists").getAsInt();
-				int leaver_status = playerObj.get("leaver_status").getAsInt();
+				int leaver_status = 0;
+				if (playerObj.has("leaver_status")) {
+					leaver_status = playerObj.get("leaver_status").getAsInt();
+				}
 				int gold = playerObj.get("gold").getAsInt();
 				int last_hits = playerObj.get("last_hits").getAsInt();
 				int denies = playerObj.get("denies").getAsInt();
@@ -140,18 +157,132 @@ public class Personal {
 				int tower_damage = playerObj.get("tower_damage").getAsInt();
 				int hero_healing = playerObj.get("hero_healing").getAsInt();
 				int level = playerObj.get("level").getAsInt();
-				JsonArray abilities = playerObj.getAsJsonArray("ability_upgrades");
-				for (JsonElement ability_upgrade_elem : abilities) {
-					JsonObject ability_upgrade = ability_upgrade_elem.getAsJsonObject();
-					int ability = ability_upgrade.get("ability").getAsInt();
-					int time = ability_upgrade.get("time").getAsInt();
-					int levell = ability_upgrade.get("level").getAsInt();
+				int playerKey = 0;
+				try {
+					String query = "INSERT INTO player (account_id, player_slot, hero_id, item_0, item_1, item_2, item_3, item_4, item_5, kills, deaths, assists, leaver_status, gold, last_hits, denies, gold_per_min, xp_per_min, gold_spent, hero_damage, tower_damage, hero_healing, level) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+					PreparedStatement ps = (PreparedStatement) con
+							.prepareStatement(query,
+									Statement.RETURN_GENERATED_KEYS);
+					ps.setInt(1, account_id);
+					ps.setInt(2, player_slot);
+					ps.setInt(3, hero_id);
+					ps.setInt(4, item_0);
+					ps.setInt(5, item_1);
+					ps.setInt(6, item_2);
+					ps.setInt(7, item_3);
+					ps.setInt(8, item_4);
+					ps.setInt(9, item_5);
+					ps.setInt(10, kills);
+					ps.setInt(11, deaths);
+					ps.setInt(12, assists);
+					ps.setInt(13, leaver_status);
+					ps.setInt(14, gold);
+					ps.setInt(15, last_hits);
+					ps.setInt(16, denies);
+					ps.setInt(17, gold_per_min);
+					ps.setInt(18, xp_per_min);
+					ps.setInt(19, gold_spent);
+					ps.setInt(20, hero_damage);
+					ps.setInt(21, tower_damage);
+					ps.setInt(22, hero_healing);
+					ps.setInt(23, level);
+					ps.executeUpdate();
+					ResultSet rs = ps.getGeneratedKeys();
+					rs.next();
+					playerKey = rs.getInt(1);
+					playerKeys[keyPos] = playerKey;
+				} catch (SQLException e) {
+					System.out.println("Statement error.");
+					e.printStackTrace();
 				}
+				if (playerObj.has("ability_upgrades")) {
+					JsonArray abilities = playerObj
+							.getAsJsonArray("ability_upgrades");
+					for (JsonElement ability_upgrade_elem : abilities) {
+						JsonObject ability_upgrade = ability_upgrade_elem
+								.getAsJsonObject();
+						int ability = ability_upgrade.get("ability").getAsInt();
+						int time = ability_upgrade.get("time").getAsInt();
+						int levell = ability_upgrade.get("level").getAsInt();
+						String query = "INSERT INTO ability_upgrades (id, level, time, ability) VALUES (?, ?, ?, ?)";
+						try {
+							PreparedStatement ps = (PreparedStatement) con
+									.prepareStatement(query);
+							ps.setInt(1, playerKey);
+							ps.setInt(2, levell);
+							ps.setInt(3, time);
+							ps.setInt(4, ability);
+							ps.executeUpdate();
+						} catch (SQLException e) {
+							System.out
+									.println("Could not generate ability_upgrade statement.");
+							e.printStackTrace();
+						}
+					}
+				}
+				keyPos++;
+			}
+			boolean radiant_win = resultObj.get("radiant_win").getAsBoolean();
+			int duration = resultObj.get("duration").getAsInt();
+			int start_time = resultObj.get("start_time").getAsInt();
+			int match_id = resultObj.get("match_id").getAsInt();
+			int match_seq_num = resultObj.get("match_seq_num").getAsInt();
+			int tower_status_radiant = resultObj.get("tower_status_radiant")
+					.getAsInt();
+			int tower_status_dire = resultObj.get("tower_status_dire")
+					.getAsInt();
+			int barracks_status_radiant = resultObj.get(
+					"barracks_status_radiant").getAsInt();
+			int barracks_status_dire = resultObj.get("barracks_status_dire")
+					.getAsInt();
+			int cluster = resultObj.get("cluster").getAsInt();
+			int first_blood_time = resultObj.get("first_blood_time").getAsInt();
+			int lobby_type = resultObj.get("lobby_type").getAsInt();
+			int human_players = resultObj.get("human_players").getAsInt();
+			int leagueid = resultObj.get("leagueid").getAsInt();
+			int positive_votes = resultObj.get("positive_votes").getAsInt();
+			int negative_votes = resultObj.get("negative_votes").getAsInt();
+			int game_mode = resultObj.get("game_mode").getAsInt();
+			String query = "INSERT INTO matches (radiant_win, duration, start_time, match_id, match_seq_num, tower_status_radiant, tower_status_dire, barracks_status_radiant, barracks_status_dire, cluster, first_blood_time, lobby_type, human_players, leagueid, positive_votes, negative_votes, game_mode, player_one, player_two, player_three, player_four, player_five, player_six, player_seven, player_eight, player_nine, player_ten) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			try {
+				PreparedStatement ps = (PreparedStatement) con
+						.prepareStatement(query);
+				ps.setBoolean(1, radiant_win);
+				ps.setInt(2, duration);
+				ps.setInt(3, start_time);
+				ps.setInt(4, match_id);
+				ps.setInt(5, match_seq_num);
+				ps.setInt(6, tower_status_radiant);
+				ps.setInt(7, tower_status_dire);
+				ps.setInt(8, barracks_status_radiant);
+				ps.setInt(9, barracks_status_dire);
+				ps.setInt(10, cluster);
+				ps.setInt(11, first_blood_time);
+				ps.setInt(12, lobby_type);
+				ps.setInt(13, human_players);
+				ps.setInt(14, leagueid);
+				ps.setInt(15, positive_votes);
+				ps.setInt(16, negative_votes);
+				ps.setInt(17, game_mode);
+				ps.setInt(18, playerKeys[0]);
+				ps.setInt(19, playerKeys[1]);
+				ps.setInt(20, playerKeys[2]);
+				ps.setInt(21, playerKeys[3]);
+				ps.setInt(22, playerKeys[4]);
+				ps.setInt(23, playerKeys[5]);
+				ps.setInt(24, playerKeys[6]);
+				ps.setInt(25, playerKeys[7]);
+				ps.setInt(26, playerKeys[8]);
+				ps.setInt(27, playerKeys[9]);
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println("Match statement failed.");
+				e.printStackTrace();
 			}
 		}
 	}
-	
-	/* Return a json object from the  given url. */
+
+	/* Return a json object from the given url. */
 	public static JsonObject getJsonRequest(String url) {
 		System.out.println(url);
 		JsonObject obj = new JsonObject();
